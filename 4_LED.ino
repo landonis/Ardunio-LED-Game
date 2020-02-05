0d3346fc903552af50081ccb8b985266e6abcbe2
landonis <43480252+landonis@users.noreply.github.com>
2020-02-05 16:00:50 -0500

    changed method for reading a win.
    New method for checking for wins, it now will use an interrupt to check if the green light is lit. If it is it stores it into a bool and checks for the win when the light turns off.

--- 4_LED.ino
+++ 4_LED.ino
@@ -1,17 +1,18 @@
 // 2930 bytes
 //2916 bytes
+//2670 bytes
 
 int timeTill = 400;
 
-const int buttonPin = 3;
-const int victoryLight = 2;
-const int light1 = 9;
-const int light2 = 10;
-const int lightGreen = 11;
-const int light3 = 12;
-const int light4 = 13;
+const byte buttonPin = 3;
+const byte victoryLight = 2;
+const byte light1 = 9;
+const byte light2 = 10;
+const byte lightGreen = 11;
+const byte light3 = 12;
+const byte light4 = 13;
 
-int buttonState = 0;
+bool isLit = false;
 bool Pressed = false;
 void setup() {
   pinMode(victoryLight, OUTPUT);
@@ -21,27 +22,32 @@ void setup() {
   pinMode(lightGreen, OUTPUT);
   pinMode(light3, OUTPUT); 
   pinMode(light4, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), VictoryCheck, RISING);
   Serial.begin(9600);
 }
   
 void loop() {
-  Pressed = false;
   digitalWrite(victoryLight, LOW);
   digitalWrite(light1, HIGH);
+  
   delay(timeTill);
+  
   digitalWrite(light1, LOW);
   digitalWrite(light2, HIGH);
-  buttonStatus();
+
   delay(timeTill);
-  buttonStatus();
+
   digitalWrite(light2, LOW);
   digitalWrite(lightGreen, HIGH);
-  Serial.print(buttonState);
-  VictoryCheck();
-  delay(timeTill/2);
-  VictoryCheck();
-  delay(timeTill/2);
-  VictoryCheck();
+
+
+  delay(timeTill);
+  if(isLit)
+  {
+    Winner();
+    isLit = false;
+    return;
+  }
   digitalWrite(lightGreen, LOW);
   digitalWrite(light3, HIGH);
   delay(timeTill);
@@ -51,47 +57,35 @@ void loop() {
   digitalWrite(light4, LOW);
   delay(timeTill);
 }
-void buttonStatus()
+void Winner()
 {
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH)
+  digitalWrite(victoryLight, HIGH);
+  timeTill = (timeTill/2)+20;
+  int flashDelay = 200;
+  Serial.print("VICTORY");
+  digitalWrite(2, HIGH);
+  for(int i=0; i<10; i+=1)
   {
-    Pressed = true;
-  }
-  else
-  {
-    Pressed = false;
-  }
+      digitalWrite(light2, LOW);
+      digitalWrite(light3, LOW);
+      digitalWrite(light1, HIGH);
+      digitalWrite(light4, HIGH);
+      delay(flashDelay);
+      digitalWrite(light1, LOW);
+      digitalWrite(light4, LOW);
+      digitalWrite(light2, HIGH);
+      digitalWrite(light3, HIGH);
+      delay(flashDelay);
+   }
+   digitalWrite(lightGreen, LOW);
+   digitalWrite(light1, LOW);
+   digitalWrite(light2, LOW);
+   digitalWrite(light3, LOW);
+   digitalWrite(light4, LOW);
+   digitalWrite(victoryLight, LOW);
 }
+
 void VictoryCheck()
 {
-  buttonState = digitalRead(buttonPin);
-  if(buttonState == HIGH && Pressed == false )
-  {
-    timeTill = (timeTill/2)+50;
-      int flashDelay = 200;
-      Serial.print("VICTORY");
-      digitalWrite(2, HIGH);
-      for(int i=0; i<10; i+=1)
-      {
-        digitalWrite(10, LOW);
-        digitalWrite(12, LOW);
-        digitalWrite(9, HIGH);
-        digitalWrite(13, HIGH);
-        delay(flashDelay);
-        digitalWrite(9, LOW);
-        digitalWrite(13, LOW);
-        digitalWrite(10, HIGH);
-        digitalWrite(12, HIGH);
-        delay(flashDelay);
-      }
-      digitalWrite(2, LOW);
-      digitalWrite(9, LOW);
-      digitalWrite(10, LOW);
-      digitalWrite(11, LOW);
-      digitalWrite(12, LOW);
-      digitalWrite(13, LOW);
-        
-    return;
-  }
+  isLit = digitalRead(lightGreen) == HIGH;
 }
